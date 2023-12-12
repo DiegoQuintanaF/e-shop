6878e57eea330be851a4b8f4b873fab81ed19af8
Diego Quintana <dquintana@utb.edu.co>
2023-12-12 05:51:20 -0500

    feat: The code was refactored

--- src/network/routes.js
+++ src/network/routes.js
@@ -1,24 +1,11 @@
 import express from 'express'
-import { search } from '../components/search/search.model.js'
+import { routerPage } from '../components/page/page.network.js'
+import { routerSearch } from '../components/search/search.network.js'
 
 export const routerApp = (app) => {
-  const views = express.Router()
-  const api = express.Router()
+  const routerApi = express.Router()
+  routerApi.use('/search', routerSearch)
 
-  app.use('/', routerViews(views))
-  app.use('/api/v1', routerApi(api))
-}
-
-function routerViews(router) {
-  router.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-  return router
-}
-
-function routerApi(router) {
-  router.get('/search', search)
-
-  return router
+  app.use('/', routerPage)
+  app.use('/api/v1', routerApi)
 }
