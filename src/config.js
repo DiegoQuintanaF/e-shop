11c01f565ba45e422de4a6489bc8b58791ec57de
Diego Quintana <dquintana@utb.edu.co>
2023-12-12 05:22:43 -0500

    feat: Now you can obtain the absolute path of the project

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 import 'dotenv/config'
+import { join } from 'node:path'
+
+const currentDirectory = new URL('.', import.meta.url).pathname
+const projectRoot = join(currentDirectory, '..')
 
 export default {
   port: process.env.PORT || 3000,
@@ -6,5 +10,6 @@ export default {
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
+  projectRoot
 }
