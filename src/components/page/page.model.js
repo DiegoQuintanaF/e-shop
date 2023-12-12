9ebb56a7a99e38a6b1eec6f146cfaa921dd0eaa1
Diego Quintana <dquintana@utb.edu.co>
2023-12-12 05:56:20 -0500

    fix: Removal of obsolete comments

--- src/components/page/page.model.js
+++ src/components/page/page.model.js
@@ -1,8 +1,4 @@
-// import { join } from 'node:path'
-// import config from './../../config.js'
-
 const index = (req, res) => {
-  // res.render(join(config.projectRoot, 'views', 'index.ejs'))
   res.render('index.ejs')
 }
 
