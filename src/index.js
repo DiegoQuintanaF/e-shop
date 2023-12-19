c6d5129c2f298598f96542a2b808b1d3a51dd0e4
Diego Quintana <dquintana@utb.edu.co>
2023-12-19 11:11:55 -0500

    feat: docker, swagger more

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import express from 'express'
 import config from './config.js'
 import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js'
 import { routerApp } from './network/routes.js'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
+
+const swaggerDocument = YAML.load(`${config.projectRoot}/swagger.yaml`)
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 routerApp(app)
 
 app.all('*', notFoundHandler)
