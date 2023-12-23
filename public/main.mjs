d3090925f314ecf2e70c32ddd63cc6a4a6a28fc9
Diego Quintana <dquintana@utb.edu.co>
2023-12-23 11:12:22 -0500

    feat: There is now a connection from the front-end to the back-end.

--- public/main.mjs
+++ public/main.mjs
@@ -1,3 +1,36 @@
 import './scripts/navbar.js'
-import './scripts/products-container.js'
+import { clickCard } from './scripts/products-container.js'
 import './scripts/product-info.js'
+
+const fecthProducts = async () => {
+  const response = await fetch('/api/v1/products')
+  const products = await response.json()
+  return products
+}
+
+const renderProducts = async () => {
+  const { products } = await fecthProducts()
+  console.log(await products)
+
+  const productInfo = document.querySelector('.products__container')
+  productInfo.innerHTML = products.map(product => `
+    <div class="product-card__container" id="${product._id}">
+      <img class="product-card__img"
+        src="${product.images[0]}" alt="${product.description}"
+        srcset="">
+      <div class="product-card__info">
+        <div>
+          <p class="product-card__price">$ ${product.price},00</p>
+          <h4 class="product-card__name">${product.name}</h4>
+        </div>
+        <div class="product-card__add-shopping-cart"></div>
+      </div>
+    </div> `
+  ).join('')
+
+  clickCard()
+}
+
+(async () => {
+  await renderProducts()
+})()
--- public/scripts/products-container.js
+++ public/scripts/products-container.js
@@ -1,20 +1,29 @@
-const productCards = document.querySelectorAll('.product-card__container')
-const productInfo = document.querySelector('.product-info__container')
+const clickCard = () => {
+  const productCards = document.querySelectorAll('.product-card__container')
+  const productInfo = document.querySelector('.product-info__container')
 
-const link = (e) => {
-  return () => {
-    const id = e.id
-    console.log(id)
-    window.location.href = `/#click-in-${id}`
-    productInfo.showModal()
-    productInfo.scrollTo(0, 0)
+  const link = (card) => {
+    return () => {
+      const id = card.id
+      window.location.href = `/#click-in-${id}`
+      productInfo.querySelector('.product-info__img').src = card.children[0].src
+      productInfo.querySelector('.product-info__img').alt = card.children[0].alt
+
+      productInfo.querySelector('.product-info__bottom-name').textContent = card.children[1].children[0].children[1].textContent
+      productInfo.querySelector('.product-info__bottom-price').textContent = card.children[1].children[0].children[0].textContent
+      productInfo.querySelector('.product-info__bottom-description').innerHTML = productInfo.querySelector('.product-info__img').alt
+      productInfo.showModal()
+      productInfo.scrollTo(0, 0)
+    }
+  }
+
+  for (const productCard of productCards) {
+    const title = productCard.children[0]
+    title.addEventListener('click', link(productCard))
+
+    const tit = productCard.children[1].children[0]
+    tit.addEventListener('click', link(productCard))
   }
 }
 
-for (const productCard of productCards) {
-  const title = productCard.children[0]
-  title.addEventListener('click', link(productCard))
-
-  const tit = productCard.children[1].children[0]
-  tit.addEventListener('click', link(productCard))
-}
+export { clickCard }
