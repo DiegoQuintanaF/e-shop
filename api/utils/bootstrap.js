1f33e8459738a63eddd229b14b21c1e150995553
Diego Quintana <dquintana@utb.edu.co>
2023-12-23 11:07:32 -0500

    feat: Update of the database import to db/index.js.

--- api/utils/bootstrap.js
+++ api/utils/bootstrap.js
@@ -1,8 +1,8 @@
-import { connect } from '../../db/mongodb/index.js'
+import { db } from '../../db/index.js'
 
 const bootstrap = async (app, config) => {
   console.log('[bootstrap] Starting server...')
-  await connect()
+  await db.connect()
 
   app.listen(config.port, () => {
     console.log(`[bootstrap] Server listening on ${config.host}:${config.port}`)
