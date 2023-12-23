1e1359ebf0a0e4f9de57c3484b644345c2531146
Diego Quintana <dquintana@utb.edu.co>
2023-12-23 10:53:00 -0500

    feat: Initial controllers for /orders and /products were added

--- api/network/routes.js
+++ api/network/routes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { routerPage } from '../components/page/page.network.js'
 import { routerSearch } from '../components/search/search.network.js'
-import { routerOrder } from '../components/order/order.network.js'
+import { routerOrder } from '../components/orders/order.network.js'
+import { routerProducts } from '../components/products/products.network.js'
 
 export const routerApp = (app) => {
   const routerApi = express.Router()
-  routerApi.use('/order', routerOrder)
+  routerApi.use('/orders', routerOrder)
   routerApi.use('/search', routerSearch)
+  routerApi.use('/products', routerProducts)
 
   app.use('/', routerPage)
   app.use('/api/v1', routerApi)
